00347ad2dd07fd03bed7280d929dafde5e26f5e2
Renato Krause <renatokrause@gmail.com>
2026-07-01 19:30:12 -0300

    Move legacy Tkinter app and clean project launch scripts

--- create_airt_shortcut.ps1
+++ create_airt_shortcut.ps1
@@ -1,7 +1,7 @@
 ﻿$projectDir = Split-Path -Parent $MyInvocation.MyCommand.Path
 $shortcutPath = Join-Path $projectDir 'AIRT.lnk'
 $targetPath = Join-Path $projectDir 'AIRT.bat'
-$iconPath = Join-Path $projectDir 'assets\airt-shortcut.ico'
+$iconPath = Join-Path $projectDir 'src\airt\resources\icons\app.ico'
 
 if (Test-Path $shortcutPath) {
     Remove-Item -LiteralPath $shortcutPath -Force
@@ -11,9 +11,10 @@ $shell = New-Object -ComObject WScript.Shell
 $shortcut = $shell.CreateShortcut($shortcutPath)
 $shortcut.TargetPath = $targetPath
 $shortcut.WorkingDirectory = $projectDir
+
 if (Test-Path $iconPath) {
     $shortcut.IconLocation = "$iconPath,0"
 }
-$shortcut.Save()
 
+$shortcut.Save()
 Write-Host "Shortcut created: $shortcutPath"
